8aab7af8d4d22b594a31d302fed337c0a5c2133c
solparrondo <101563449+solparrondo@users.noreply.github.com>
2022-05-20 11:37:14 -0300

    Cubos creados y desplazamiento de Platform

---
+++ Assets/PlatformBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBehaviour : MonoBehaviour
+{
+
+    public float speed;
+    public bool toRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toRight == true)
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+        else
+        {
+            transform.position -= new Vector3(speed, 0, 0);
+        }
+
+        if (transform.position.x > 4.5f)
+        {
+            toRight = false;
+        }
+        if(transform.position.x < -4.5f)
+        {
+            toRight = true;
+        }
+    }
+}
